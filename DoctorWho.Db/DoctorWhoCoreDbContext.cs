338d687fcccf57f7946276d957b81ba880a1b671
Moayad <2001moayad@gmail.com>
2023-03-26 15:48:54 +0300

    Seed to tblEnemy

--- DoctorWho.Db/DoctorWhoCoreDbContext.cs
+++ DoctorWho.Db/DoctorWhoCoreDbContext.cs
@@ -86,7 +86,47 @@ namespace DoctorWho.Db {
             );
             #endregion
 
-            
+            #region EnemySeed
+            modelBuilder.Entity<Enemy>().HasData(
+                new Enemy {
+                    EnemyId = 1,
+                    EnemyName = "Daleks",
+                    Description = "It landed here way back in the 9th century." +
+                    " After a long; deadly battle; The Dalek was defeated." +
+                    " It’s casing was destroyed; the creature inside was cut into three pieces so it could" +
+                    " never reform. Two were successfully transported away and guarded by the order of the" +
+                    " custodians. But one piece was left behind. It stayed in Sheffield; where it was found" +
+                    " 12 centuries later and was accidentally woken up. You would think a squiddy thing like" +
+                    " that couldn’t get far. But turns out it could hitch a ride on humans. It built it’s own" +
+                    " casing from spare parts and bits of it’s original outer shell."
+                },
+                new Enemy {
+                    EnemyId = 2,
+                    EnemyName = "Krasko",
+                    Description = "Krasko was released from prison and travelled back in time to change the past."
+                },
+                new Enemy {
+                    EnemyId = 3,
+                    EnemyName = "The Eaters of Light",
+                    Description = "A mythical Pictish Beast; discovered by the Doctor to be all too real…"
+                },
+                new Enemy {
+                    EnemyId = 4,
+                    EnemyName = "Hydroflax",
+                    Description = "A malevolent cyborg known for ending battles by eating his enemies dead or" +
+                    " alive." +
+                    " Don’t lose your head!"
+                },
+                new Enemy {
+                    EnemyId = 5,
+                    EnemyName = "Flemming",
+                    Description = "A maitre d’ who can make any problem disappear." +
+                    " Even if he has to digest passengers himself."
+                }
+            );
+            #endregion
+
+
         }
 
     }
---
+++ DoctorWho.Db/Migrations/20230326124744_Insert_tblEnemy.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DoctorWho.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class Insert_tblEnemy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "tblEnemy",
+                columns: new[] { "EnemyId", "Description", "EnemyName" },
+                values: new object[,]
+                {
+                    { 1, "It landed here way back in the 9th century. After a long; deadly battle; The Dalek was defeated. It’s casing was destroyed; the creature inside was cut into three pieces so it could never reform. Two were successfully transported away and guarded by the order of the custodians. But one piece was left behind. It stayed in Sheffield; where it was found 12 centuries later and was accidentally woken up. You would think a squiddy thing like that couldn’t get far. But turns out it could hitch a ride on humans. It built it’s own casing from spare parts and bits of it’s original outer shell.", "Daleks" },
+                    { 2, "Krasko was released from prison and travelled back in time to change the past.", "Krasko" },
+                    { 3, "A mythical Pictish Beast; discovered by the Doctor to be all too real…", "The Eaters of Light" },
+                    { 4, "A malevolent cyborg known for ending battles by eating his enemies dead or alive. Don’t lose your head!", "Hydroflax" },
+                    { 5, "A maitre d’ who can make any problem disappear. Even if he has to digest passengers himself.", "Flemming" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "tblEnemy",
+                keyColumn: "EnemyId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "tblEnemy",
+                keyColumn: "EnemyId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "tblEnemy",
+                keyColumn: "EnemyId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "tblEnemy",
+                keyColumn: "EnemyId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "tblEnemy",
+                keyColumn: "EnemyId",
+                keyValue: 5);
+        }
+    }
+}
