e57fd039cb996f774b5da1d14671c0087e70d665
Moayad <2001moayad@gmail.com>
2023-03-26 13:26:30 +0300

    Add Db Sets

--- DoctorWho.Db/DoctorWhoCoreDbContext.cs
+++ DoctorWho.Db/DoctorWhoCoreDbContext.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using DoctorWho.Db.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DoctorWho.Db {
-    public class DoctorWhoCoreDbContext {
+    public class DoctorWhoCoreDbContext : DbContext {
+        public DbSet<Author> tblAuthor { get; set; }
+        public DbSet<Companion> tblCompanion { get; set; }
+        public DbSet<Doctor> tblDoctor { get; set; }
+        public DbSet<Enemy> tblEnemy { get; set; }
+        public DbSet<Episode> tblEpisode { get; set; }
+        public DbSet<EpisodeCompanion> tblEpisodeCompanion { get; set; }
+        public DbSet<EpisodeEnemy> tblEpisodeEnemy { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-MKSIC0H\\sqlexpress;Initial Catalog=DoctorWhoCore;Integrated Security=True; TrustServerCertificate=True");
+        }
     }
 }
