604c200add23a493ea7080379d19fe4168fdc1ab
Moayad <2001moayad@gmail.com>
2023-03-27 14:25:21 +0300

    Create migration for spSummariseEpisodes stored procedure

---
+++ DoctorWho.Db/Migrations/20230327110729_spSummariseEpisodes.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoctorWho.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class spSummariseEpisodes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"CREATE PROCEDURE spSummariseEpisodes AS
+                    SELECT TOP 3 CompanionName, COUNT(CompanionName) AS NumberOfAppearence
+                    FROM viewEpisodes
+                    GROUP BY CompanionName
+                    ORDER BY COUNT(CompanionName) DESC
+                    SELECT TOP 3 EnemyName, COUNT(EnemyName) AS NumberOfAppearence
+                    FROM viewEpisodes 
+                    GROUP BY EnemyName
+                    ORDER BY COUNT(EnemyName) DESC";
+
+            migrationBuilder.Sql(sp);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
