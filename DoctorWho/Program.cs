7465c88683fbbc3f557c1e91773dae26c0d6fb90
Moayad <2001moayad@gmail.com>
2023-03-27 13:39:57 +0300

    Create method to execute fnEnemies function

--- DoctorWho/Program.cs
+++ DoctorWho/Program.cs
@@ -9,16 +9,27 @@ namespace DoctorWho
     public class Program {
         static void Main(string[] args) {
             var fnCompanions = companionFunctionClass();
+            Console.WriteLine("----------------");
+            var fnEnemies = enemiesFunctionClass();
         }
 
         private static List<fnCompanionClass> companionFunctionClass() {
+            int count = 1;
             var context = new DoctorWhoCoreDbContext();
             var query = from c in context.fnCompanions(1) select c;
             foreach(var c in query) {
-                Console.WriteLine(c.CompanionName);
+                Console.WriteLine($"Companion {count} Name: {c.CompanionName}");
+                count++;
             }
             return query.ToList();
         }
-        
+        private static List<fnEnemiesClass> enemiesFunctionClass() {
+            var context = new DoctorWhoCoreDbContext();
+            var query = from e in context.fnEnemies(1) select e;
+            foreach (var e in query) {
+                Console.WriteLine($"Enemy Name: {e.EnemyName}");
+            }
+            return query.ToList();
+        }
     }
 }
