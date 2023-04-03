06164106c202698cb3646e195527c6866dcc0074
Moayad <2001moayad@gmail.com>
2023-04-03 15:53:29 +0300

    Create Episode services

--- DoctorWho.Db/Services/EpisodeServices.cs
+++ DoctorWho.Db/Services/EpisodeServices.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using DoctorWho.Db.IRepositories;
+using DoctorWho.Db.IServices;
+using DoctorWho.Db.Models;
+using DoctorWho.Db.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,44 @@ using System.Threading.Tasks;
 
 namespace DoctorWho.Db.Services
 {
-    public class EpisodeServices
+    public class EpisodeServices : IEpisodeServices
     {
+        IGenericRepository<Episode> repoEpisode = new GenericRepository<Episode>();
+        IEpisodeRepository repoEpisode2 = new EpisodeRepository();
+
+        public void AddCompanionToEpisode(int companionId, int episodeId)
+        {
+            repoEpisode2.AddCompanionToEpisode(companionId, episodeId);
+        }
+
+        public void AddEnemyToEpisode(int enemyId, int episodeId)
+        {
+            repoEpisode2.AddEnemyToEpisode(enemyId, episodeId);
+        }
+
+        public void DeleteEpisode(int id)
+        {
+            repoEpisode.Delete(id);
+        }
+
+        public IEnumerable<Episode> GetAllEpisodes()
+        {
+            return repoEpisode.GetAll();
+        }
+
+        public Episode GetEpisodeById(int id)
+        {
+            return repoEpisode.GetById(id);
+        }
+
+        public void InsertEpisode(Episode episode)
+        {
+            repoEpisode.Insert(episode);
+        }
+
+        public void UpdateEpisode(Episode episode)
+        {
+            repoEpisode.Update(episode);
+        }
     }
 }
