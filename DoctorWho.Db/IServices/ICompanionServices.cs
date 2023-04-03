79491006808725acc67f62e23bfb41cc32a0ec4e
Moayad <2001moayad@gmail.com>
2023-04-03 15:16:41 +0300

    Create Companion services

---
+++ DoctorWho.Db/IServices/ICompanionServices.cs
@@ -0,0 +1,22 @@
+﻿using DoctorWho.Db.Functions;
+using DoctorWho.Db.IRepositories;
+using DoctorWho.Db.Models;
+using DoctorWho.Db.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorWho.Db.IServices
+{
+    public interface ICompanionServices
+    {
+        IEnumerable<Companion> GetAllCompanions();
+        Companion GetCompanionById(int id);
+        void InsertCompanion(Companion companion);
+        void DeleteCompanion(int id);
+        void UpdateCompanion(Companion companion);
+        List<fnCompanionClass> companionFunctionClass(int episodeId);
+    }
+}
