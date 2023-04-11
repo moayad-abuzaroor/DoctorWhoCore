c5d7eae7aea235985f6d4cf5bc7994299a83ca81
Moayad <2001moayad@gmail.com>
2023-04-11 13:31:07 +0300

    Create Episode response class

---
+++ DoctorWho.Db/Persistence/Services/Communication/EpisodeResponse.cs
@@ -0,0 +1,20 @@
+﻿using DoctorWho.Db.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoctorWho.Db.Persistence.Services.Communication
+{
+    public class EpisodeResponse : BaseResponse
+    {
+        public Episode Episode { get; set; }
+        public EpisodeResponse(bool success, string message, Episode episode) : base(success, message)
+        {
+            Episode = episode;
+        }
+        public EpisodeResponse(Episode episode) : this(true, string.Empty, episode) { }
+        public EpisodeResponse(string message) : this(false, message, null) { }
+    }
+}
