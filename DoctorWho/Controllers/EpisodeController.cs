3d0a02e4d1c20b79f56a6a4a02c81db4ae827fc9
Moayad <2001moayad@gmail.com>
2023-04-11 14:09:41 +0300

    Create Enemy controller file

---
+++ DoctorWho/Controllers/EpisodeController.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DoctorWho.Db.Domain.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoctorWho.Controllers
+{
+    [Route("/api/[controller]")]
+    public class EpisodeController : Controller
+    {
+        private readonly IEpisodeServices _episodeServices;
+        private readonly IMapper _mapper;
+        public EpisodeController(IEpisodeServices episodeServices, IMapper mapper)
+        {
+            _episodeServices = episodeServices;
+            _mapper = mapper;
+        }
+    }
+}
